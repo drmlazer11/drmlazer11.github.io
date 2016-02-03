13310674f835a3c2ed5a79054b66da5cb1d748ae
drmlazer11 <davidrmuench@gmail.com>
2016-02-03 17:45:09 -0500

    Update upvote.js

--- js/upvote.js
+++ js/upvote.js
@@ -57,9 +57,9 @@ var Upvote = (function (window, document) {
         	
         		pulsing_circle_interval = setInterval(function() {
         		
-        			$('#loading-circle').css('opacity', '.7');
+        			// $('#loading-circle').css('opacity', '.7');
         		
-        			/*$('#L').css('opacity', '.8');
+        			$('#L').css('opacity', '.8');
         			$('#L').css('-webkit-transition', '-webkit-transform .5s');
         			$('#L').css('-webkit-transform', 'scale (.9)');
         		
@@ -98,7 +98,7 @@ var Upvote = (function (window, document) {
         			}, 700);*/
         		
         			// $('#loading-circle').css('background-color', arr_colors[int_loading_count]);
-        			/*int_loading_count += 1;
+        			int_loading_count += 1;
         			if (int_loading_count == 4) {
         				clearInterval(pulsing_circle_interval);
         				setTimeout(function() {
@@ -109,11 +109,11 @@ var Upvote = (function (window, document) {
         					$('#content').fadeIn(300);
         					$('#content').addClass('animated slideInUp');
         				}, 750);
-        			}*/
+        			}
         			
-        			setTimeout(function() {
+        			/*setTimeout(function() {
         				$('#loading-circle').css('opacity', '1');
-        			}, 500);
+        			}, 500);*/
         			
         		}, 1000);
         
